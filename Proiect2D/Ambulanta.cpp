035533e68a7141613a969f6ac14603fd301f878a
AlexMincu <minculescualex98@gmail.com>
2022-05-22 21:19:28 +0300

    fixed ambulanta position

--- Proiect2D/Ambulanta.cpp
+++ Proiect2D/Ambulanta.cpp
@@ -18,14 +18,14 @@ void Ambulanta::draw() {
 	glPushMatrix();
 
 	//glBegin(GL_POLYGON);
-	glTranslatef(x, y[0], 0.6);
+	glTranslatef(x, y[0], 0.3);
 	glScalef(.5, .5, .5);
 
 	glRotatef(90, 1, 0, 0);
-	glRotatef(90, 0, 1, 0);
+	glRotatef(-90, 0, 1, 0);
 
 	l->draw("ambulanta");
-	glRotated(90, 0, 0, 1);
+	//glRotated(90, 0, 0, 1);
 	//glVertex3f(3.5 + x, 0.4 + y[0], 0.1);// Stanga jos
 	//glVertex3f(3.5 + x, -0.4 + y[0], 0.1); // Dreapta jos
 	//glVertex3f(5.5 + x, -0.4 + y[0], 0.7); // Dreapta sus
